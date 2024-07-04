37315d3d24597ce6d5a98fc7698c7fa2321799c2
Blake Eriks <blakeeriks.dev@gmail.com>
2024-07-04 09:22:51 +0200

    Fix user route

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,31 +1,15 @@
-import Header from '@/components/Header'
-import { type MetaFunction } from '@remix-run/node'
-import { Outlet } from '@remix-run/react'
+import { LoaderFunction, type MetaFunction } from '@remix-run/node'
+import { redirect } from '@remix-run/react'
 // import Header from '~/components/Header'
 
 export const meta: MetaFunction = () => {
   return [{ title: 'New Remix App' }, { name: 'description', content: 'Welcome to Remix!' }]
 }
 
-// export const loader: LoaderFunction = async ({ request }) => {
-//   await requireUserId(request)
-//   return redirect('/home')
-// }
+export const loader: LoaderFunction = async ({ request }) => {
+  return redirect('/quotes')
+}
 
 export default function Index() {
-  // const user = useAtomValue(userAtom)
-  // const navigate = useNavigate()
-  // const location = useLocation()
-
-  // useEffect(() => {
-  //   if (!user) navigate('/users')
-  // }, [user, navigate])
-
-  return (
-    <div className='font-sans p-4'>
-      <Header />
-      <ul className='list-disc mt-4 pl-6 space-y-2'>This is where the rendered quotes will be.</ul>
-      <Outlet />
-    </div>
-  )
+  return null
 }
--- app/routes/users/index.tsx
+++ app/routes/users/index.tsx
@@ -1,9 +1,40 @@
 import UserPanel from '@/components/UserPanel'
-import { getUsers } from '@/db/user.db'
-import { json } from '@remix-run/node'
-import { useLoaderData } from '@remix-run/react'
+import { createUser, getUsers } from '@/db/user.db'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { ActionFunctionArgs, json } from '@remix-run/node' // or cloudflare/deno
+import { redirect, useLoaderData } from '@remix-run/react'
+import { getValidatedFormData } from 'remix-hook-form'
+import * as Zod from 'zod'
 
 export const loader = async () => json(await getUsers())
+
+const schema = Zod.object({
+  name: Zod.string().min(2, {
+    message: 'Name must be at least 2 characters.',
+  }),
+})
+type FormData = Zod.infer<typeof schema>
+
+const resolver = zodResolver(schema)
+
+export const action = async ({ request }: ActionFunctionArgs) => {
+  const {
+    errors,
+    data,
+    receivedValues: defaultValues,
+  } = await getValidatedFormData<FormData>(request, resolver)
+
+  if (errors) {
+    return json({ errors, defaultValues })
+  }
+
+  try {
+    await createUser(data)
+    return redirect('/users')
+  } catch (error) {
+    return json({ errorMessage: 'Failed to create user' }, { status: 400 })
+  }
+}
 
 export default function Index() {
   const users = useLoaderData<typeof loader>()
