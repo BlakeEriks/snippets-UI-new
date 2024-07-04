c3d0db7fc71dd49cb12bad3fc56f606fc9255b2e
Blake Eriks <blakeeriks.dev@gmail.com>
2024-07-04 09:22:33 +0200

    Delete

--- app/routes/asdusers._index.tsx
+++
@@ -1,8 +0,0 @@
-// export const loader: LoaderFunction = async ({ request }) => {
-//   await requireUserId(request)
-//   return redirect('/home')
-// }
-
-export default function Index() {
-  return <div>users</div>
-}
