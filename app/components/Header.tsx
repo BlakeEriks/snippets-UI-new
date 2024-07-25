2403bf719df6455f1a00204a10c851d19009c2b8
Blake Eriks <blakeeriks.dev@gmail.com>
2024-07-25 21:25:12 +0200

    Save progress

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,46 +1,31 @@
+import { cn } from '@/lib/styles'
 import { modalStateAtom } from '@/state/modal'
 import userAtom from '@/state/user'
-import { Link } from '@remix-run/react'
+import { Link, useLocation } from '@remix-run/react'
 import { useAtomValue, useSetAtom } from 'jotai'
-import { PlusIcon, SmileIcon, UploadIcon } from 'lucide-react'
+import { ArrowLeftRight, PlusIcon, SmileIcon, UploadIcon } from 'lucide-react'
 import { Tooltip } from './ui/tooltip'
 
 const Header = () => {
   const user = useAtomValue(userAtom)
+  const { pathname } = useLocation()
+
   // const { handleUpload, dummyRequest } = useFileUpload()
   // const location = window.location.pathname
   const setEditQuoteModalState = useSetAtom(modalStateAtom('editQuote'))
 
   return (
     <header className='flex flex-row w-full justify-between py-2 px-4 border-b'>
-      <Link to='/quotes' className='flex'>
+      <Link to='/books' className={cn('flex', pathname.includes('books') && 'pointer-events-none')}>
         <h1 className='text-4xl italic font-semibold'>Quotes</h1>
         <h1 className='text-4xl italic font-light opacity-50'> - the app</h1>
       </Link>
       <div className='flex items-center gap-2'>
-        {/* <Tooltip placement='bottom' title='Home'>
-            <Button className={cn(location === '/' && 'border')}>
-              <HomeIcon />
-            </Button>
-          </Tooltip>
-        </Link> */}
-        {/* <Link
-          to={`random/${new Date().getTime()}`}
-          className={cn(location.includes('random') && 'border')}
-        >
-          <Tooltip placement='bottom' title='Random Quote'>
-            <Button className=''>
-              <ArrowLeftRight />
-            </Button>
-          </Tooltip>
-        </Link> */}
-        {/* <Link to='staging'>
-          <Tooltip placement='bottom' title='Quote Staging'>
-            <Button className=''>
-              <Edit />
-            </Button>
-          </Tooltip>
-        </Link> */}
+        <Tooltip content='Random Quote' asChild>
+          {/* <Link to={`random/${new Date().getTime()}`}> */}
+          <ArrowLeftRight />
+          {/* </Link> */}
+        </Tooltip>
         <Tooltip content='Upload Snippets'>
           {/* <Upload
             beforeUpload={handleUpload}
@@ -54,14 +39,11 @@ const Header = () => {
           {/* </Upload> */}
         </Tooltip>
         <Tooltip content='Add Quote' onClick={() => setEditQuoteModalState({})}>
-          {/* <Button className='' onClick={() => setEditQuoteModalState({})}> */}
           <PlusIcon />
-          {/* </Button> */}
         </Tooltip>
         <Link to='users'>
           <Tooltip content='Change User'>
             {user ? user.name[0].toUpperCase() : <SmileIcon />}
-            {/* <Button className='w-12'></Button> */}
           </Tooltip>
         </Link>
       </div>
--- app/components/UserPanel.tsx
+++ app/components/UserPanel.tsx
@@ -1,21 +1,11 @@
 import { User } from '@prisma/client'
-import { Form, useSearchParams } from '@remix-run/react'
+import { Form, Link, useSearchParams } from '@remix-run/react'
 import { useSetAtom } from 'jotai'
 import _ from 'lodash'
 import { useNavigate } from 'react-router-dom'
 import { useRemixForm } from 'remix-hook-form'
 import userAtom from '../state/user'
 import { Button } from './ui/button'
-import {
-  Dialog,
-  DialogContent,
-  DialogFooter,
-  DialogHeader,
-  DialogTitle,
-  DialogTrigger,
-} from './ui/dialog'
-import { Input } from './ui/input'
-import { Label } from './ui/label'
 
 type UserPanelProps = {
   users: User[]
@@ -30,7 +20,7 @@ const UserPanel = ({ users }: UserPanelProps) => {
     const user = _.find(users, { id })
     if (user) {
       setUser(user)
-      navigate('/')
+      navigate('/books')
     }
   }
 
@@ -47,16 +37,19 @@ const UserPanel = ({ users }: UserPanelProps) => {
     <div className='m-[10%]'>
       <h1 className='text-2xl text-center my-6'>Whos Using Quotes?</h1>
       <div className='flex justify-center'>
-        {users.map(({ name, id }: User) => (
-          <Button
-            key={id}
-            className='text-center w-32 mx-2 py-12 border border-slate-300 rounded-3xl hover:shadow-lg transition-shadow cursor-pointer'
-            onClick={() => selectUser(id)}
-          >
-            {name}
-          </Button>
-        ))}
-        <Dialog
+        <Form method='post'>
+          {users.map(({ name, id }: User) => (
+            <Button
+              key={id}
+              className='text-center w-32 mx-2 py-12 border border-slate-300 rounded-3xl hover:shadow-lg transition-shadow cursor-pointer'
+              onClick={() => selectUser(id)}
+            >
+              {name}
+            </Button>
+          ))}
+        </Form>
+        <Link to='./new'>+</Link>
+        {/* <Dialog
           onOpenChange={open => {
             reset()
             setSearchParams(open ? { newUser: 'true' } : {})
@@ -72,21 +65,21 @@ const UserPanel = ({ users }: UserPanelProps) => {
             </Button>
           </DialogTrigger>
           <DialogContent>
-            <Form method='post' onSubmit={handleSubmit}>
+            <Form method='post' onSubmit={handleSubmit} className='space-y-4'>
               <DialogHeader>
                 <DialogTitle>New User</DialogTitle>
               </DialogHeader>
               <Label>
                 Name:
                 <Input type='text' {...register('name')} />
-                {errors.name && <p>{errors.name.message}</p>}
+                {errors.name && <p className='text-red-500'>{errors.name.message}</p>}
               </Label>
               <DialogFooter>
                 <Button type='submit'>Save changes</Button>
               </DialogFooter>
             </Form>
           </DialogContent>
-        </Dialog>
+        </Dialog> */}
       </div>
     </div>
   )
